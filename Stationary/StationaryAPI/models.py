1893916e950cf209ed7758b5fa47d7700b2ee2d2
Anshu Musaddi <anshumusaddi@gmail.com>
2018-12-08 17:06:45 +0530

    First

---
+++ Stationary/StationaryAPI/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+class Item(models.Model):
+    PID = models.BigAutoField(primary_key=True)
+    Perishable = models.BooleanField(null=False,default=False)
+    Name = models.CharField(max_length = 100)
+    OnHold = models.PositiveIntegerField()
+    MinQty = models.PositiveIntegerField()
+    Qty = models.PositiveIntegerField()
+
+class Faculty(models.Model):
+    user = models.OneToOneField(User,related_name='profile',on_delete=models.CASCADE)
+    FID = models.CharField(max_length=100,primary_key=True)
+    CurrIt = models.ManyToManyField(Item,through = 'CurrentItems',through_fields=('faculty', 'item'))
+
+class Vendor(models.Model):
+    VID = models.CharField(max_length=100,primary_key=True)
+    Name = models.CharField(max_length = 100)
+    DealsIn = models.ManyToManyField(Item,through = 'Dealer',through_fields=('vendor', 'item'))
+
+class Dealer(models.Model):
+    vendor = models.ForeignKey(Vendor,on_delete=models.CASCADE)
+    item = models.ForeignKey(Item,on_delete=models.CASCADE)
+    price = models.DecimalField()
+
+class CurrentItems(models.Model):
+    faculty = models.ForeignKey(Faculty,on_delete=models.CASCADE)
+    item = models.ForeignKey(Item,on_delete=models.CASCADE)
+    Qty = models.PositiveIntegerField()
