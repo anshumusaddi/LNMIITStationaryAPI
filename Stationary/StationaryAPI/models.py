ecc25e502ead5979da080d8fb7462eb2b2507c0d
Anshu Musaddi <anshumusaddi@gmail.com>
2018-12-08 17:44:36 +0530

    dB Complete

--- Stationary/StationaryAPI/models.py
+++ Stationary/StationaryAPI/models.py
@@ -11,14 +11,14 @@ class Item(models.Model):
     Qty = models.PositiveIntegerField()
 
 class Faculty(models.Model):
-    user = models.OneToOneField(User,related_name='profile',on_delete=models.CASCADE)
     FID = models.CharField(max_length=100,primary_key=True)
-    CurrIt = models.ManyToManyField(Item,through = 'CurrentItems',through_fields=('faculty', 'item'))
+    user = models.OneToOneField(User,related_name='profile',on_delete=models.CASCADE)
+    CurrIt = models.ManyToManyField(Item,related_name='curritems',through = 'CurrentItems',through_fields=('faculty', 'item'))
 
 class Vendor(models.Model):
     VID = models.CharField(max_length=100,primary_key=True)
     Name = models.CharField(max_length = 100)
-    DealsIn = models.ManyToManyField(Item,through = 'Dealer',through_fields=('vendor', 'item'))
+    DealsIn = models.ManyToManyField(Item,related_name='deal',through = 'Dealer',through_fields=('vendor', 'item'))
 
 class Dealer(models.Model):
     vendor = models.ForeignKey(Vendor,on_delete=models.CASCADE)
@@ -27,5 +27,33 @@ class Dealer(models.Model):
 
 class CurrentItems(models.Model):
     faculty = models.ForeignKey(Faculty,on_delete=models.CASCADE)
+    item = models.ForeignKey(Item,on_delete=models.CASCADE)
+    Qty = models.PositiveIntegerField()
+
+class Order(models.Model):
+    OrderID = models.BigAutoField(primary_key=True)
+    faculty = models.ForeignKey(Faculty,related_name='fact',on_delete=models.CASCADE)
+    items = models.ManyToManyField(Item,related_name='orditems',through = 'OrderItems',through_fields=('oid', 'item'))
+    Approved = models.BooleanField(null=True)
+    Delivered = models.BooleanField(null=False,default=False)
+    OrderDate = models.DateField(auto_now_add=True)
+    DeliveryDate = models.DateField()
+
+class OrderItems(models.Model):
+    oid = models.ForeignKey(Order,on_delete=models.CASCADE)
+    item = models.ForeignKey(Item,on_delete=models.CASCADE)
+    Qty = models.PositiveIntegerField()
+
+class SupplyOrder(models.Model):
+    OrderID = models.BigAutoField(primary_key=True)
+    vendor = models.ForeignKey(Vendor,related_name='vend',on_delete=models.CASCADE)
+    items = models.ManyToManyField(Item,related_name='supplyorditems',through = 'SupplyOrderItems',through_fields=('oid', 'item'))
+    OrderDate = models.DateField(auto_now_add=True)
+    DeliveryDate = models.DateField()
+    Paid = models.BooleanField(null=False,default=False)
+    Delivered = models.BooleanField(null=False,default=False)
+
+class SupplyOrderItems(models.Model):
+    oid = models.ForeignKey(SupplyOrder,on_delete=models.CASCADE)
     item = models.ForeignKey(Item,on_delete=models.CASCADE)
     Qty = models.PositiveIntegerField()
